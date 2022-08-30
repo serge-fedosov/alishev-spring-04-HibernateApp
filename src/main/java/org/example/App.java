856762f7bf7e7768e4c9b2d2ff3cb0ced1774bc5
Serge Fedosov <107842308+serge-fedosov@users.noreply.github.com>
2022-08-30 21:36:05 +0400

    07/05 HQL FROM Person where age > 30

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -17,7 +17,7 @@ public class App {
         try {
             session.beginTransaction();
 
-            List<Person> people = session.createQuery("FROM Person").getResultList();
+            List<Person> people = session.createQuery("FROM Person where age > 30").getResultList();
 
             for (Person person : people) {
                 System.out.println(person);
