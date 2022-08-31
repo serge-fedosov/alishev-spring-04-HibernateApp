5a936915ebb5943571e94bf3d016524b2cd4f67d
Serge Fedosov <107842308+serge-fedosov@users.noreply.github.com>
2022-08-31 22:55:13 +0400

    07/11 lazy load

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,34 +1,35 @@
 package org.example;
 
-import org.example.model.Actor;
-import org.example.model.Movie;
+import org.example.model.Item;
+import org.example.model.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 
 public class App {
     public static void main( String[] args ) {
-        Configuration configuration = new Configuration().addAnnotatedClass(Actor.class).addAnnotatedClass(Movie.class);
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class).addAnnotatedClass(Item.class);
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
 
-        try (sessionFactory) {
-            Session session = sessionFactory.getCurrentSession();
+        try {
             session.beginTransaction();
 
-            Actor actor = session.get(Actor.class, 2);
-            System.out.println(actor.getMovies());
+            Person person = session.get(Person.class, 1);
+            System.out.println("Получили человека");
 
-            Movie movieToRemove = actor.getMovies().get(0);
-
-            actor.getMovies().remove(0);
-            movieToRemove.getActors().remove(actor);
+            // Получим связанные сущности (Lazy)
+            System.out.println(person.getItems());
 
             session.getTransaction().commit();
+            // session.close() вызывается автоматически после коммита
+
+        } finally {
+            sessionFactory.close();
         }
     }
 }
--- src/main/java/org/example/model/Person.java
+++ src/main/java/org/example/model/Person.java
@@ -1,13 +1,14 @@
 package org.example.model;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.Cascade;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @Entity
-@Table(name = "Actor")
-public class Actor {
+@Table(name = "Person")
+public class Person {
 
     @Id
     @Column(name = "id")
@@ -20,18 +21,14 @@ public class Actor {
     @Column(name = "age")
     private int age;
 
-    @ManyToMany
-    @JoinTable(
-            name = "Actor_Movie",
-            joinColumns = @JoinColumn(name = "actor_id"),
-            inverseJoinColumns = @JoinColumn(name = "movie_id")
-    )
-    private List<Movie> movies;
+    @OneToMany(mappedBy = "owner")
+    @Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
+    private List<Item> items;
 
-    public Actor() {
+    public Person() {
     }
 
-    public Actor(String name, int age) {
+    public Person(String name, int age) {
         this.name = name;
         this.age = age;
     }
@@ -60,30 +57,26 @@ public class Actor {
         this.age = age;
     }
 
-    public List<Movie> getMovies() {
-        return movies;
+    public List<Item> getItems() {
+        return items;
     }
 
-    public void setMovies(List<Movie> movies) {
-        this.movies = movies;
+    public void setItems(List<Item> items) {
+        this.items = items;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Actor actor = (Actor) o;
-        return id == actor.id && age == actor.age && Objects.equals(name, actor.name);
-    }
+    public void addItem(Item item) {
+        if (this.items == null) {
+            this.items = new ArrayList<>();
+        }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, age);
+        this.items.add(item);
+        item.setOwner(this);
     }
 
     @Override
     public String toString() {
-        return "Actor{" +
+        return "Person{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", age=" + age +
