abdafdcd067110a6c89adc9e7ff2ffc7a9ffd7f1
Serge Fedosov <107842308+serge-fedosov@users.noreply.github.com>
2022-08-31 22:13:05 +0400

    07/10 movie.getActors()

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -20,18 +20,8 @@ public class App {
             Session session = sessionFactory.getCurrentSession();
             session.beginTransaction();
 
-            Movie movie = new Movie("Pulp fiction", 1994);
-            Actor actor1 = new Actor("Harvey Keitel", 81);
-            Actor actor2 = new Actor("Samuel L. Jackson", 72);
-
-            movie.setActors(new ArrayList<>(List.of(actor1, actor2)));
-
-            actor1.setMovies(new ArrayList<>(Collections.singletonList(movie)));
-            actor2.setMovies(new ArrayList<>(Collections.singletonList(movie)));
-
-            session.save(movie);
-            session.save(actor1);
-            session.save(actor2);
+            Movie movie = session.get(Movie.class, 1);
+            System.out.println(movie.getActors());
 
             session.getTransaction().commit();
         }
